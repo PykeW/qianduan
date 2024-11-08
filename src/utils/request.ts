2b368495c418a0eed32286fea9090cbd9bca0e6d
lujie <768885817@qq.com>
2024-11-08 01:46:11 +0000

    4

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,29 +1,34 @@
-import axios from 'axios'
+import axios from "axios";
 
 const request = axios.create({
-  baseURL: 'https://vwswgnefsnhk.sealosgzg.site/api',
-  timeout: 15000,
+  // 使用环境变量中的 API URL
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
   }
-})
+});
 
 // 请求拦截器
 request.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
+  (config) => {
+    // 可以在这里添加认证 token 等
+    return config;
   },
-  error => Promise.reject(error)
-)
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // 响应拦截器
 request.interceptors.response.use(
-  response => response.data,
-  error => Promise.reject(error)
-)
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    console.error('请求错误:', error);
+    return Promise.reject(error);
+  }
+);
 
-export default request 
+export default request; 
